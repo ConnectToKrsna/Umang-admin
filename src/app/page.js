f51c033cb8567a1c652b31cb155fe237056611f2
ConnectToKrsna <connecttokrishnanow@gmail.com>
2024-07-11 16:46:20 +0530

    update4

--- src/app/page.js
+++ src/app/page.js
@@ -7,8 +7,13 @@ export default function Home() {
     <main>
     <h1>Hare Krishna</h1>
 
+    <div>
     <Link href="/register">Admin Page</Link>
+    </div>
+
+    <div>
     <Link href="/addUser">Registration Page</Link>
+    </div>
     </main>
   );
 }
