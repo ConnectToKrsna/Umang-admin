3bd6ee21d05bb030bdcffc59364ec894d1a13aef
ConnectToKrsna <87686171+ConnectToKrsna@users.noreply.github.com>
2024-07-22 16:37:03 +0530

    Update style.css in the page

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,6 +1,7 @@
 'use client'
 import { useState, useEffect } from "react";
 import Link from "next/link";
+import "../style.css"
 
 // Fetch registration data from the server
 const getRegister = async () => {
